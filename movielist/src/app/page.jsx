48eaa43da1822c908f81e4d8a70c8fa8665b8e41
Muhammad Rizky Ferdiansyah <muhammadrizkyferdiansyah@gmail.com>
2023-10-18 17:14:40 +0700

    add app/page

---
+++ movielist/src/app/page.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import logo from "@/assets/logo.png";
+import Link from "next/link";
+
+export default function Home() {
+  return (
+    <>
+      <header className="py-2 shadow-md shadow-gray-600">
+        <nav className="flex container mx-auto justify-between items-center">
+          <Image src={logo} alt="logo" width="100" height="100" />
+          <div>
+            <input type="text" placeholder="Searching" />
+          </div>
+          <div>
+            <Link href="/signin">Sign In</Link>
+          </div>
+        </nav>
+      </header>
+      <div></div>
+    </>
+  );
+}
